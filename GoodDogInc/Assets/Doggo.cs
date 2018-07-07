96362004d1c850ec8e8123dd0082f983b2ba82dc
Dylan Simmer <goldenoblivion@gmail.com>
2018-07-07 16:26:21 +1000

    directional animation

--- GoodDogInc/Assets/Doggo.cs
+++ GoodDogInc/Assets/Doggo.cs
@@ -5,14 +5,40 @@ using UnityEngine;
 public class Doggo : MonoBehaviour {
     public Vector2 target;
     public float speed;
+    public Animator ani;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        ani = GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = (target - (Vector2)transform.position).normalized * speed + (Vector2)transform.position;
+        var left = 2;
+        var right = 3;
+        var forward = 0;
+        var back = 1;
+        var direction = (target - (Vector2)transform.position);
+
+        if (direction.x > direction.y) {
+            if (direction.x > 0)
+            {
+                ani.SetInteger("state", right);
+            } else
+            {
+                ani.SetInteger("state", left);
+            }
+        } else
+        {
+            if (direction.y > 0)
+            {
+                ani.SetInteger("state", back);
+            }
+            else
+            {
+                ani.SetInteger("state", forward);
+            }
+        }
+        transform.position = direction.normalized * speed + (Vector2)transform.position;
     }
 }
