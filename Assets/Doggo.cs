71a5f41880aadeed66b653c5c47ecf3922146ec4
Dylan Simmer <goldenoblivion@gmail.com>
2018-07-07 22:46:44 +1000

    update animations and added interact and bark

--- Assets/Doggo.cs
+++ Assets/Doggo.cs
@@ -7,7 +7,7 @@ public class Doggo : Movement
 {
     private Transform leash;
     private Human myHuman;
-
+    private Boolean interrupted;
     private void Start()
     {
         leash = transform.Find("Leash");
@@ -16,7 +16,7 @@ public class Doggo : Movement
 	// Update is called once per frame
 	override protected void FixedUpdate ()
 	{
-        if (!ani.GetBool("fight")) {
+        if (!ani.GetBool("fight") && !ani.GetBool("interact") && !ani.GetBool("bark")) {
             base.FixedUpdate();
         }
     }
@@ -36,6 +36,7 @@ public class Doggo : Movement
     }
 
     public void Fight(Enemy enemy) {
+        Debug.Log("Reaaaaddyyyyy... FIGHT");
         Destroy(enemy.gameObject);
         ani.SetBool("fight", true);
         StartCoroutine(FightAni());
@@ -45,4 +46,40 @@ public class Doggo : Movement
         yield return new WaitForSeconds(1);
         ani.SetBool("fight", false);
     }
+    public void InteractWithDoggo(PlaceOfInterest POI) {
+        Debug.Log("Interact");
+        ani.SetBool("interact", true);
+        StartCoroutine(InteractWithDoggoAni(POI));
+    }
+
+    public IEnumerator InteractWithDoggoAni(PlaceOfInterest POI) {
+        yield return new WaitForSeconds(1);
+        if (!interrupted) {
+            POI.Progress();
+            ani.SetBool("interact", false);
+        }
+        interrupted = false;
+    }
+    public void DoggoBark() {
+        ani.SetBool("bark", true);
+        Debug.Log("Bark");
+        StartCoroutine(DoggoBarkAni());
+    }
+
+    public IEnumerator DoggoBarkAni() {
+        yield return new WaitForSeconds(1);
+        ani.SetBool("bark", false);
+    }
 }
+
+// In case we want raycasting later
+// if (Input.GetMouseButton(0)) {
+//     RaycastHit2D hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 0);
+//     if (hit) {
+//         if (hit.collider.CompareTag("POI")) {
+//                 Debug.Log("This is a POI");
+//         }
+//     }
+//     // doggo.target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+//     // doggo.target.z = 0f;
+// }
---
+++ Assets/PlaceOfInterest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceOfInterest : MonoBehaviour {
+    public Doggo doggo;
+    public State progress;
+
+    public enum State
+    {
+        zero,
+        half,
+        full
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate ()
+	{
+        if (progress != State.full && (doggo.transform.position - transform.position).magnitude < 0.5) {
+            doggo.InteractWithDoggo(this);
+        }
+    }
+
+    public void Progress() {
+        if (progress == State.zero) {
+            progress = State.half;
+        } else if (progress == State.half) {
+            progress = State.full;
+        }
+    }
+}
